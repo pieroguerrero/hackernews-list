47c7982447083c5903f0ba33ea598c402dd037a8
Piero Guerrero <piero.guerrero@gmail.com>
2023-03-25 17:25:33 -0500

    Add configuration values file
    Several configuration values were added to the project

---
+++ src/configs/ConfigValues.ts
@@ -0,0 +1,11 @@
+/**
+ * Provides configuration constant values for the application.
+ */
+const ConfigValues = Object.freeze({
+  QueryURLParameter: "query",
+  PageURLParameter: "page",
+  PostQueryURL: "https://hn.algolia.com/api/v1/search_by_date",
+  LocalStorageId: "user-id-liked-posts",
+});
+
+export default ConfigValues;
